5af355dbb7de2be6ed787ccfba6e1fe7b2ae53a1
Connor <csims9@hotmail.com>
2023-07-13 10:21:03 -0700

    Add globals

---
+++ src/globals/Globals.js
@@ -0,0 +1,6 @@
+const APP_INFO = {
+    title: 'React Dictionary',
+    copyright: 2023,
+};
+const API_KEY_DICTIONARY = '9da84366-baf0-49d0-abae-0b9a0a5db70b';
+const API_KEY_THESAURUS = 'f1cd9d58-309b-420d-a225-46016bfe3ab6';
