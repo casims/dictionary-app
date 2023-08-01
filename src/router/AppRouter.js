c152b888a173215ccbac483ee4f7c73b2c7b388c
Connor <csims9@hotmail.com>
2023-08-01 11:36:46 -0700

    Link Header component

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//Components
+import Header from '../components/Header';
+
 //Pages
 import PageMain from '../pages/PageMain';
 import PageSingleWord from '../pages/PageSingleWord';
@@ -9,6 +12,7 @@ function AppRouter() {
 
     return (
         <BrowserRouter>
+            <Header/>
             <main>
                 <Routes>
                     <Route path="/" exact element={<PageMain />}/>
