74d97c18cc04ad72443a96f4517859493f61c3fd
Connor <csims9@hotmail.com>
2023-08-02 12:50:29 -0700

    Create search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchBar() {
+
+    const [searchInput, setSearchInput] = useState("");
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (searchInput === '') {
+            return;
+        } else {
+            navigate(`/word/${searchInput}`);
+        };
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <label for="word">Search for Word:</label>
+            <input type="text" value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
+            <input type="submit" value="Submit"/>
+        </form>
+    );
+};
+
+export default SearchBar;
