9269f5b95f11733c36291ae594821b0de4aa1e51
Connor <csims9@hotmail.com>
2023-08-03 11:58:57 -0700

    Reformat dictionary html structure

--- src/components/DictDefSection.js
+++ src/components/DictDefSection.js
@@ -1,5 +1,3 @@
-// import DictDefSubSection from "./DictDefSubSection";
-
 function DictDefSection({singleDefinition}) {
 
     const wordStems = singleDefinition.meta.stems;
@@ -7,30 +5,31 @@ function DictDefSection({singleDefinition}) {
 
     return(
         <section>
-            <h3>{singleDefinition.hwi.hw}</h3>
-            <p>&#40;{singleDefinition.fl}&#41;</p>
-            {singleDefinition.meta.offensive === true && <p>This word is considered offensive.</p>}
-            <ul>
-                {wordStems.map(singleWordStem => 
-                <li>
-                    <p>{singleWordStem}</p>
-                </li>)}
-            </ul>
-            <ul>
-                {wordDefinitions.map(singleWordDefinition => 
-                <li>
-                    <p>{singleWordDefinition}</p>
-                </li>)}
-            </ul>
-            <h4>Etymology</h4>
-            {singleDefinition.et ?
-                <>
-                <p>{singleDefinition.et[0][1]}</p>
-                <p>{singleDefinition.et.date}</p>
-                </>
-            :
-                <p>{singleDefinition.date}</p>
-            }
+            <section className="word-dict-information">
+                <h3>{singleDefinition.hwi.hw}</h3>
+                <p>&#40;{singleDefinition.fl}&#41;</p>
+                {singleDefinition.meta.offensive === true && <p>This word is considered offensive.</p>}
+            </section>
+            <section className="word-dict-stems">
+                <ul>
+                    {wordStems.map(singleWordStem => 
+                    <li>
+                        <p>{singleWordStem}</p>
+                    </li>)}
+                </ul>
+            </section>
+            <section className="word-dict-definitions">
+                <ul>
+                    {wordDefinitions.map(singleWordDefinition => 
+                    <li>
+                        <p>{singleWordDefinition}</p>
+                    </li>)}
+                </ul>
+            </section>
+            <section className="word-dict-etymology">
+                {singleDefinition.et && <p>{singleDefinition.et[0][1]}</p>};
+                {singleDefinition.date && <p>{singleDefinition.date}</p> };
+            </section>
         </section>
     )
 };
