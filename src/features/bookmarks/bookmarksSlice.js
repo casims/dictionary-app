485ab39f4d2383e556c6b1e3a395c24fe7d13257
Connor <csims9@hotmail.com>
2023-08-08 12:00:57 -0700

    Create bookmarksSlice.js

---
+++ src/features/bookmarks/bookmarksSlice.js
@@ -0,0 +1,42 @@
+import { APP_STORAGE_NAME } from '../../globals/Globals';
+import { createSlice } from '@reduxjs/toolkit';
+
+function getBookmarks() {
+    let bookmarksFromStorage = localStorage.getItem(APP_STORAGE_NAME);
+    if (bookmarksFromStorage === null) {
+        bookmarksFromStorage = [];
+    } else {
+        bookmarksFromStorage = JSON.parse(bookmarksFromStorage);
+    };
+    return bookmarksFromStorage;
+};
+
+const initialState = {
+    items: getBookmarks()
+};
+
+function getIndex(item, array) {
+    return array.findIndex(arrayItem => arrayItem.id === item.id);
+};
+
+export const bookmarksSlice = createSlice({
+    name: 'bookmarks',
+    initialState,
+    reducers: {
+        addBookmark: (state, action) => {
+            const newBookmarks = [...state.items, action.payload];
+            localStorage.setItem(APP_STORAGE_NAME, JSON.stringify(newBookmarks));
+            state.items = newBookmarks;
+        },
+        deleteBookmark: (state, action) => {
+            const itemsCopy = state.items;
+            itemsCopy.splice(getIndex(action.payload, state.items), 1);
+            localStorage.setItem(APP_STORAGE_NAME, JSON.stringify(itemsCopy));
+            state.items = itemsCopy;
+        }
+    },
+});
+
+export const { addBookmark, deleteBookmark } = bookmarksSlice.actions
+
+export default bookmarksSlice.reducer;
