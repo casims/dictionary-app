329b7f858523ca213dcfd42d09958cf36c516def
Connor <csims9@hotmail.com>
2023-08-26 12:40:02 -0700

    Add functionality for dropdown menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import SearchBar from './SearchBar';
-import MenuButton from '../assets/menu_icon.svg'
+import MenuButton from '../assets/menu_icon.svg';
 
 function Header() {
+
+    const [menuExpanded, setMenuExpanded] = useState(false);
+
+    function toggleMenu() {
+        setMenuExpanded((current) => !current);
+    };
+
+    useEffect(() => {
+        if (menuExpanded) {
+            document.getElementById('dropdown-menu').style.height = '10rem';
+        } else {
+            document.getElementById('dropdown-menu').style.height = 0;
+        }
+    }, [menuExpanded]);
+
     return (
         <header>
             <nav className='desktop-nav'>
@@ -11,11 +27,11 @@ function Header() {
                         <NavLink to='/bookmarks'>Bookmarks</NavLink>
                         <NavLink to='/about'>About</NavLink>
                     </div>
-                <button>
+                <button onClick={toggleMenu}>
                     <img className="menu-button" src={MenuButton} alt=''/>
                 </button>
             </nav>
-            <nav className='mobile-nav'>
+            <nav className='mobile-nav' id='dropdown-menu'>
                 <ul>
                     <li>
                         <NavLink to='/bookmarks'>Bookmarks</NavLink>
