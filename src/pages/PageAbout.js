0a7f293618363f13898620e29d7225a5b599bff8
Connor <csims9@hotmail.com>
2023-08-30 12:52:56 -0700

    Update PageAbout.js

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -9,9 +9,8 @@ function PageAbout() {
       }, []);
 
     return(
-        <section className="content-card">
+        <section className="content-card about-card">
             <h2>About</h2>
-            <p>This product uses the Merriam-Webster API but is not endorsed or certified by Merriam-Webster.</p>
             <p>The “React Dictionary” is an application that allows users to easily search, favorite, find definitions, find synonyms, and get other valuable information about almost any word across hundreds of thousands of entries.</p>
             <p>This application uses both the Merriam-Webster's Collegiate&reg; Dictionary API as well as Merriam-Webster's Collegiate&reg; Thesaurus API.</p>
             <img src={mwlogo} alt="Merriam-Webster company logo"/>
