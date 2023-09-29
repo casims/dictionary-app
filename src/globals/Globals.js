23fc312b20e2d20d5c7053fbc3fa6ea4b26bc848
Connor <101660028+casims@users.noreply.github.com>
2023-09-29 11:06:45 -0700

    Delete src/globals/Globals.js

--- src/globals/Globals.js
+++
@@ -1,6 +0,0 @@
-export const APP_TITLE = 'React Dictionary';
-export const APP_CREATOR = 'Connor Sims';
-export const COPYRIGHT_DATE = '2023';
-export const APP_STORAGE_NAME = 'react-dictionary-bookmarks-ade';
-export const API_KEY_DICTIONARY = '9da84366-baf0-49d0-abae-0b9a0a5db70b';
-export const API_KEY_THESAURUS = 'f1cd9d58-309b-420d-a225-46016bfe3ab6';
